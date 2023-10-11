3b980551177608a9751e2ad08a67bd0625377b98
viajook <jook@via.dk>
2023-10-11 19:13:06 +0200

    gRPC Spring Boot Project: Client - first commit

---
+++ src/main/java/via/sdj3/grpcclientexample/GrpcClientExampleApplication.java
@@ -0,0 +1,12 @@
+package via.sdj3.grpcclientexample;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcClientExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcClientExampleApplication.class, args);
+    }
+}
---
+++ src/test/java/via/sdj3/grpcclientexample/GrpcClientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package via.sdj3.grpcclientexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcClientExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
